b8c467898a54ec26b0c77ab57d1496ff34f13643
松本英人 <hide@matsumotohideto-no-MacBook-Pro.local>
2020-08-31 23:24:56 +0900

    HTMLの作成

--- Chapter1/helloworld.com/myprofile/views.py
+++ Chapter1/helloworld.com/myprofile/views.py
@@ -4,7 +4,33 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def top(request):
-    return HttpResponse('トップページです!!!')
+    return HttpResponse("""
+    <!DOCTYPE html>
+    <html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Taro's Profile</title>
+    </head>
+    <body>
+    <header>Taro's Profile</header>
+
+    <nav>
+        <a href="/">TOP</a>
+        <a href="/resume">Resume</a>
+    </nav>
+
+    <h1>私についての概要</h1>
+    <p>東京に住んでいる、40歳になったエンジニアです。</p>
+
+    <h2>趣味</h2>
+    <ul>
+        <li>映画鑑賞</li>
+    </ul>
+    他の色々なコンテンツ...
+    </body>
+    </html>
+    """)
 
 def resume(request):
     return HttpResponse('職務経歴ページです!!!')
