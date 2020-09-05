84094699f749d3f1258b3b3ce4d43e06e9b49a3e
松本英人 <hide@matsumotohideto-no-MacBook-Pro.local>
2020-09-05 22:30:38 +0900

    render関数に変更

--- Chapter1/helloworld.com/myprofile/views.py
+++ Chapter1/helloworld.com/myprofile/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 
 def top(request):
     context = {
         'name': 'たろう',
     }
-    html = loader.render_to_string('myprofile/top.html', context=context, request=request)
+    html = render(request,'myprofile/top.html', context)
     return HttpResponse(html)
 
 def resume(request):
