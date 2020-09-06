07c2fb93731a32f8e0c4315518359e9428d196dd
松本英人 <hide@matsumotohideto-no-MacBook-Pro.local>
2020-09-06 11:30:52 +0900

    URLハードコーディングをなくすためのapp_nameの漏れ

--- Chapter1/helloworld.com/myprofile/urls.py
+++ Chapter1/helloworld.com/myprofile/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name= 'myprofile'
+
 urlpatterns = [
-    path('', views.top),
-    path('resume/', views.resume,),
+    path('', views.top, name='top'),
+    path('resume/', views.resume, name='resume'),
 ]
